81883da09188f9731fd1c46623c7618fabc22e55
André Teves <andreteves@gmail.com>
2015-01-19 22:50:37 -0200

    attr_accessible

--- app/models/newsletter_email.rb
+++ app/models/newsletter_email.rb
@@ -1,3 +1,3 @@
 class NewsletterEmail < ActiveRecord::Base
-  attr_accessible :email, :utm_source, :utm_campaign, :utm_medium
+  attr_accessible :email, :utm_source, :utm_campaign, :utm_medium, :profile
 end
